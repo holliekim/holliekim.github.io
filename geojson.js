b0694804b26003532254b7f8ac70f38b6a3a147d
holliekim <hollieauch@gmail.com>
2017-09-26 20:47:46 -0500

    leaflet geojson

---
+++ geojson.js
@@ -0,0 +1,16 @@
+var test = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.0996036529541,
+          44.95324312432404
+        ]
+      }
+    }
+  ]
+}];
---
+++ geojsontest.js
@@ -0,0 +1,12 @@
+var geojsontest = {
+    "type": "Feature",
+    "properties": {
+        "name": "WCOB",
+        "amenity": "MN Gov Building",
+        "popupContent": "MN"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-93.0996036529541, 44.95324312432404]
+    }
+}; 
